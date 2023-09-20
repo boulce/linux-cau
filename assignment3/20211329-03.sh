1c8c4a40f19f60fa106cbfec431a8fea97c6b7ff
boulce <boulce.email@gmail.com>
2023-09-20 18:25:20 +0900

    exceptin handler

--- assignment3/20211329-03.sh
+++ assignment3/20211329-03.sh
@@ -1,3 +1,29 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+       echo "first parameter is empty"
+       exit 0
+fi
+
+if [ $1 -le 0  ]
+then
+	echo "first parameter is not positive number"
+	exit 0
+fi
+
+if [ -z $2 ]
+then
+	echo "second parameter is empty"
+	exit 0
+fi
+
+if [ $2 -le 0 ]
+then
+	echo "second parameter is not positive number"
+	exit 0
+fi
+
 for i in $(seq 1 $1)
 do
 	for j in $(seq 1 $2)
@@ -7,3 +33,4 @@ do
 	done
 	echo ""
 done
+exit 0
